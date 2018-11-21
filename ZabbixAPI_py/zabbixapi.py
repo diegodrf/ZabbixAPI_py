581eb3e5387b2ccc53365e7ae3120121ee9dc2ee
Diego Faria <diego.rdfaria@gmail.com>
2018-11-21 00:28:52 -0200

    Incluído comentários Código refatorado mais uma vez. Foram removidos os parâmetros "element" e recriados como uma variável dentro da função. Um usuário poderia utilizar uma função host, reescrevendo o campo element como user, por exemplo.

--- ZabbixAPI_py/zabbixapi.py
+++ ZabbixAPI_py/zabbixapi.py
@@ -2,6 +2,7 @@ import requests
 import json
 from colorama import Fore, Style
 
+
 class Zabbix:
     def __init__(self, server='http://URL_FOR_SERVER/zabbix'):
         self.server = '{}/api_jsonrpc.php'.format(server)
@@ -49,6 +50,12 @@ class Zabbix:
                                                             response.json()['error']['data'])
             return Fore.RED + error_message + Style.RESET_ALL
 
+    """
+    --> Função principal.
+    
+        Todos os métodos são repassados para esta função, que realiza de fato a consulta e retorna os valores
+    """
+
     def __data(self, method, query, element):
         self.data['method'] = '{}.{}'.format(element, method)
         self.data['params'] = query
@@ -60,222 +67,268 @@ class Zabbix:
                                                             response.json()['error']['data'])
             return Fore.RED + error_message + Style.RESET_ALL
 
-    def host(self, method, query=None, element='host'):
+    """
+    
+    --> Funções que facilitam o usuário consultar a API do Zabbix.
+    
+        A lógica de consulta passada no parâmetro "query" pode ser encontrada em 
+        https://www.zabbix.com/documentation/3.4/manual/api/reference 
+    """
+
+    def host(self, method, query=None):
+        element = 'host'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def history(self, method, query=None, element='history'):
+    def history(self, method, query=None):
+        element = 'history'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def trend(self, method, query=None, element='trend'):
+    def trend(self, method, query=None):
+        element = 'trend'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def event(self, method, query=None, element='event'):
+    def event(self, method, query=None):
+        element = 'event'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def problem(self, method, query=None, element='problem'):
+    def problem(self, method, query=None):
+        element = 'problem'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def host(self, method, query=None, element='host'):
+    def hostgroup(self, method, query=None):
+        element = 'hostgroup'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def hostgroup(self, method, query=None, element='hostgroup'):
+    def hostinterface(self, method, query=None):
+        element = 'hostinterface'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def hostinterface(self, method, query=None, element='hostinterface'):
+    def usermacro(self, method, query=None):
+        element = 'usermacro'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def usermacro(self, method, query=None, element='usermacro'):
+    def maintenance(self, method, query=None):
+        element = 'maintenance'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def maintenance(self, method, query=None, element='maintenance'):
+    def item(self, method, query=None):
+        element = 'item'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def item(self, method, query=None, element='item'):
+    def application(self, method, query=None):
+        element = 'application'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def application(self, method, query=None, element='application'):
+    def trigger(self, method, query=None):
+        element = 'trigger'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def trigger(self, method, query=None, element='trigger'):
+    def graph(self, method, query=None):
+        element = 'graph'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def graph(self, method, query=None, element='graph'):
+    def graphitem(self, method, query=None):
+        element = 'graphitem'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def graphitem(self, method, query=None, element='graphitem'):
+    def template(self, method, query=None):
+        element = 'template'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def template(self, method, query=None, element='template'):
+    def discoveryrule(self, method, query=None):
+        element = 'discoveryrule'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def discoveryrule(self, method, query=None, element='discoveryrule'):
+    def itemprototype(self, method, query=None):
+        element = 'itemprototype'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def itemprototype(self, method, query=None, element='itemprototype'):
+    def triggerprototype(self, method, query=None):
+        element = 'triggerprototype'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def triggerprototype(self, method, query=None, element='triggerprototype'):
+    def graphprototype(self, method, query=None):
+        element = 'graphprototype'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def graphprototype(self, method, query=None, element='graphprototype'):
+    def hostprototype(self, method, query=None):
+        element = 'hostprototype'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def hostprototype(self, method, query=None, element='hostprototype'):
+    def correlation(self, method, query=None):
+        element = 'correlation'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def correlation(self, method, query=None, element='correlation'):
+    def action(self, method, query=None):
+        element = 'action'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def action(self, method, query=None, element='action'):
+    def alert(self, method, query=None):
+        element = 'alert'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def alert(self, method, query=None, element='alert'):
+    def service(self, method, query=None):
+        element = 'service'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def service(self, method, query=None, element='service'):
+    def dashboard(self, method, query=None):
+        element = 'dashboard'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def dashboard(self, method, query=None, element='dashboard'):
+    def screen(self, method, query=None):
+        element = 'screen'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def screen(self, method, query=None, element='screen'):
+    def screenitem(self, method, query=None):
+        element = 'screenitem'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def screenitem(self, method, query=None, element='screenitem'):
+    def templatescreen(self, method, query=None):
+        element = 'templatescreen'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def templatescreen(self, method, query=None, element='templatescreen'):
+    def templatescreenitem(self, method, query=None):
+        element = 'templatescreenitem'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def templatescreenitem(self, method, query=None, element='templatescreenitem'):
+    def map(self, method, query=None):
+        element = 'map'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def map(self, method, query=None, element='map'):
+    def httptest(self, method, query=None):
+        element = 'httptest'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def httptest(self, method, query=None, element='httptest'):
+    def drule(self, method, query=None):
+        element = 'drule'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def drule(self, method, query=None, element='drule'):
+    def dcheck(self, method, query=None):
+        element = 'dcheck'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def dcheck(self, method, query=None, element='dcheck'):
+    def dhost(self, method, query=None):
+        element = 'dhost'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def dhost(self, method, query=None, element='dhost'):
+    def dservice(self, method, query=None):
+        element = 'dservice'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def dservice(self, method, query=None, element='dservice'):
+    def user(self, method, query=None):
+        element = 'user'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def user(self, method, query=None, element='user'):
+    def usergroup(self, method, query=None):
+        element = 'usergroup'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def usergroup(self, method, query=None, element='usergroup'):
+    def mediatype(self, method, query=None):
+        element = 'mediatype'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def mediatype(self, method, query=None, element='mediatype'):
+    def usermedia(self, method, query=None):
+        element = 'usermedia'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def usermedia(self, method, query=None, element='usermedia'):
+    def iconmap(self, method, query=None):
+        element = 'iconmap'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def iconmap(self, method, query=None, element='iconmap'):
+    def image(self, method, query=None):
+        element = 'image'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def image(self, method, query=None, element='image'):
+    def proxy(self, method, query=None):
+        element = 'proxy'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
 
-    def proxy(self, method, query=None, element='proxy'):
-        if query is None:
-            query = {'output': 'extend'}
-        return self.__data(method, query, element)
-
-    def script(self, method, query=None, element='script'):
+    def script(self, method, query=None):
+        element = 'script'
         if query is None:
             query = {'output': 'extend'}
         return self.__data(method, query, element)
